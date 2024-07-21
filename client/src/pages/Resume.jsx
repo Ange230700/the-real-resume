4091620e0839498d1d603eb585408617e1c9d127
Ange230700 <kouakouangeericstephane@gmail.com>
2024-07-22 00:16:03 +0200

    work...

--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -17,7 +17,7 @@ function Resume() {
               </a>
             </p>
             <p className="phone-number">
-              <a href="tel:+33751524188">+33 7 51 52 41 88</a>
+              <a href="tel:+33759802343">+33 7 59 80 23 43</a>
             </p>
             <p>44000 Nantes</p>
           </div>
@@ -94,43 +94,45 @@ function Resume() {
           expérience utilisateur. Mon objectif est de mettre mes compétences à
           profit pour participer à des solutions innovantes et impactantes.
         </p>
-        <section className="competences">
-          <h3>Compétences</h3>
+      </section>
+      <section className="competences">
+        <h3>Compétences</h3>
 
-          <h4>&nbsp;&nbsp;&nbsp;&nbsp;Tech stack</h4>
-          <ul className="competences-techniques-list">
-            <li className="competences-techniques-list-item">React</li>
-            <li className="competences-techniques-list-item">Express</li>
-            <li className="competences-techniques-list-item">Node</li>
-            <li className="competences-techniques-list-item">MySQL</li>
-          </ul>
+        <h4>&nbsp;&nbsp;&nbsp;&nbsp;Tech stack</h4>
+        <ul className="competences-techniques-list">
+          <li className="competences-techniques-list-item">React</li>
+          <li className="competences-techniques-list-item">Express</li>
+          <li className="competences-techniques-list-item">Node</li>
+          <li className="competences-techniques-list-item">MySQL</li>
+        </ul>
 
-          <h4>&nbsp;&nbsp;&nbsp;&nbsp;Outils</h4>
-          <ul className="competences-techniques-list">
-            <li className="competences-techniques-list-item">Git</li>
-            <li className="competences-techniques-list-item">VS Code</li>
-            <li className="competences-techniques-list-item">Postman</li>
-            <li className="competences-techniques-list-item">Figma</li>
-            <li className="competences-techniques-list-item">Trello / Jira</li>
-          </ul>
+        <h4>&nbsp;&nbsp;&nbsp;&nbsp;Outils</h4>
+        <ul className="competences-techniques-list">
+          <li className="competences-techniques-list-item">Git</li>
+          <li className="competences-techniques-list-item">VS Code</li>
+          <li className="competences-techniques-list-item">Postman</li>
+          <li className="competences-techniques-list-item">Figma</li>
+          <li className="competences-techniques-list-item">Trello / Jira</li>
+          <li className="competences-techniques-list-item">GPT-4o</li>
+        </ul>
 
-          <h4>&nbsp;&nbsp;&nbsp;&nbsp;Hébergement</h4>
-          <ul className="competences-techniques-list">
-            <li className="competences-techniques-list-item">WinSCP</li>
-            <li className="competences-techniques-list-item">puttY</li>
-            <li className="competences-techniques-list-item">Vercel</li>
-            <li className="competences-techniques-list-item">Render</li>
-            <li className="competences-techniques-list-item">Alwaysdata</li>
-          </ul>
+        <h4>&nbsp;&nbsp;&nbsp;&nbsp;Hébergement</h4>
+        <ul className="competences-techniques-list">
+          <li className="competences-techniques-list-item">WinSCP</li>
+          <li className="competences-techniques-list-item">puttY</li>
+          <li className="competences-techniques-list-item">Vercel</li>
+          <li className="competences-techniques-list-item">Render</li>
+          <li className="competences-techniques-list-item">Alwaysdata</li>
+        </ul>
 
-          <h4>&nbsp;&nbsp;&nbsp;&nbsp;Soft skills</h4>
-          <ul className="soft-skills-list">
-            <li className="soft-skills-list-item">Travail en équipe</li>
-            <li className="soft-skills-list-item">Autonomie</li>
-            <li className="soft-skills-list-item">Adaptabilité</li>
-            <li className="soft-skills-list-item">Gestion du temps</li>
-          </ul>
-        </section>
+        <h4>&nbsp;&nbsp;&nbsp;&nbsp;Soft skills</h4>
+        <ul className="soft-skills-list">
+          <li className="soft-skills-list-item">Travail en équipe</li>
+          <li className="soft-skills-list-item">Autonomie</li>
+          <li className="soft-skills-list-item">Adaptabilité</li>
+          <li className="soft-skills-list-item">Gestion du temps</li>
+          <li className="soft-skills-list-item">Méthode Agile/SCRUM</li>
+        </ul>
       </section>
       <section className="experiences">
         <h3>Expériences</h3>
@@ -153,6 +155,52 @@ function Resume() {
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Administrer la
             base de données pour une gestion optimale des contenus via une
             interface d'administration
+          </li>
+        </ul>
+        <h4>• Réalisation d'un site internet de recettes de cuisine</h4>
+        <ul className="experiences-list">
+          <li className="experiences-list-item">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Permettre aux
+            utilisateurs d'accéder à une grande variété de recettes de cuisine
+          </li>
+          <li className="experiences-list-item">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Implémenter le
+            filtrage via une barre de recherche et la catégorisation des
+            recettes par type, difficulté, prix, etc.
+          </li>
+          <li className="experiences-list-item">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Intégrer
+            l'authentification des utilisateurs pour un accès sécurisé et
+            personnalisé
+          </li>
+          <li className="experiences-list-item">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Permettre à
+            l'utilisateur de publier ses propres recettes
+          </li>
+        </ul>
+        <h4>• Réalisation d'une salle d'arcade numérique</h4>
+        <ul className="experiences-list">
+          <li className="experiences-list-item">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Permettre aux
+            utilisateurs d'accéder à la présentation de la salle virtuelle et de
+            ses jeux
+          </li>
+          <li className="experiences-list-item">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Intégrer
+            l'authentification des utilisateurs pour un accès sécurisé et
+            personnalisé
+          </li>
+          <li className="experiences-list-item">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Permettre à
+            l'utilisateur de consulter ses meilleurs scores
+          </li>
+          <li className="experiences-list-item">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Faire un
+            classement des utilisateurs en fonction de leurs scores
+          </li>
+          <li className="experiences-list-item">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Obtenir des
+            récompenses en fonction des scores
           </li>
         </ul>
         <h4>
@@ -237,12 +285,28 @@ function Resume() {
             </p>
             <p className="formation-date">09/2017 - 06/2019</p>
           </li>
+          <li className="formation-list-item">
+            <p className="formation-title-and-school">
+              Obtention du BAC scientifique |
+              <span className="school">Abidjan, Côte d'Ivoire</span>
+            </p>
+            <p className="formation-date">2017</p>
+          </li>
         </ul>
       </section>
 
-      {/* <!-- <section class="hobbies">
+      <section className="hobbies">
         <h3>Hobbies</h3>
-      </section> --> */}
+        <ul className="hobbies-list">
+          <li className="hobbies-list-item">Jouer au Basket-ball</li>
+          <li className="hobbies-list-item">
+            Apprendre le piano en autodidacte
+          </li>
+          <li className="hobbies-list-item">
+            Aller voir des blockbusters au cinéma
+          </li>
+        </ul>
+      </section>
     </main>
   );
 }
