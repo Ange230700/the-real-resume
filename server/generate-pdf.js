f8b9b6bef539f37918f13b7bbdfb538f7e52d57e
Ange230700 <kouakouangeericstephane@gmail.com>
2024-07-22 09:07:44 +0200

    work...

--- server/generate-pdf.js
+++ server/generate-pdf.js
@@ -6,12 +6,12 @@ const path = require("path");
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
-  await page.goto("http://localhost:5001", { waitUntil: "networkidle0" }); // Adjust the URL and port if necessary
+  await page.goto("http://vps-80a85dd9.vps.ovh.net:5003/", { waitUntil: "networkidle0" }); // Adjust the URL and port if necessary
   await page.pdf({
     path: path.join(__dirname, "resume.pdf"),
     format: "A4",
     printBackground: true,
-    scale: 0.75, // Adjust the scale as necessary to fit everything on one page
+    scale: 0.625, // Adjust the scale as necessary to fit everything on one page
   });
 
   await browser.close();
